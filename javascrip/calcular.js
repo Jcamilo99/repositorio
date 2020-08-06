afd7c83b2bc39bbfbfd9707d42f2fec59b5e3e9d
juan camilo <juancholm123@gmail.com>
2020-08-06 16:49:43 -0500

    primer parcial

---
+++ javascrip/calcular.js
@@ -0,0 +1,69 @@
+
+var n1
+var n2
+var n3
+var n4
+var n5
+var n6 
+var n7
+var  n8
+var n9
+var sumar 
+var restar 
+
+var resultado= document.getElementById('resultado');
+var codigo = document.getElementById('resultado');
+
+function mostrarcodigo(){
+resultado.value = '1620080186'
+}
+
+
+function bton1(){
+    n1 =document.getElementById('btn1').value;
+}
+
+function bton2(){
+    n2 =document.getElementById('btn2').value;
+}
+
+function bton3(){
+    n3 =document.getElementById('btn3').value;
+}
+
+function bton4(){
+    n4 =document.getElementById('btn4').value;
+}
+
+function bton5(){
+    n5 =document.getElementById('btn5').value;
+}
+
+function bton6(){
+    n6 =document.getElementById('btn6').value;
+}
+
+
+function btn7(){
+   n7 = document.getElementById('btn7').value;
+   
+}
+
+function btn8(){
+     n8 = document.getElementById('btn8').value;
+}
+
+function btn9(){
+    n9 = document.getElementById('btn8').value;
+}
+
+
+function sumar(){
+    sumar = document.getElementById('sumar').value;
+}
+
+function restar(){
+    restar = document.getElementById('restar').value;
+}
+
+
